9a738cedc73a73ad39e9b24a3fb9d5a08d8fce60
Ivan <ivanrakov540@gmail.com>
2024-11-05 05:21:09 +0500

    feat: add users sort by id

--- src/app/internal/users/domain/services.py
+++ src/app/internal/users/domain/services.py
@@ -77,15 +77,15 @@ class UserService:
 
     async def get_unverified_users(self):
         unverified_users = await self.user_repo.get_all_by_fields(is_verified=False)
-        return unverified_users
+        return sorted(unverified_users, key=lambda obj: obj.id, reverse=True)
 
     async def get_user_by_id(self, user_id: int):
         user = await self.user_repo.get_by_id(id=user_id)
         return user
 
     async def get_users(self):
-        user = await self.user_repo.get_all()
-        return user
+        users = await self.user_repo.get_all()
+        return sorted(users, key=lambda obj: obj.id, reverse=True)
 
     async def get_user_orders(self, user_id: int):
         user = await self.user_repo.get_user_with_related(user_id=user_id)
